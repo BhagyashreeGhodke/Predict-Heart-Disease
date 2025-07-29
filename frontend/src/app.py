ce81cdda686a18a9b8aecb5ce67cc762e93bcb70
BhagyashreeGhodke <drbpghodke@gmail.com>
2025-07-30 00:17:39 +0530

    added public port

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -159,5 +159,8 @@ def predict():
 def dashboard():
     return render_template("dashboard.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
