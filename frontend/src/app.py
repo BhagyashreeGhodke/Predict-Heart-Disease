cd19506499eb6bb59d1fbdec37570eafa50dcc6d
BhagyashreeGhodke <drbpghodke@gmail.com>
2025-08-07 00:20:43 +0530

    updated

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 # Automatically check and create the database table if needed
-db.initialize_database()
+#db.initialize_database()
 
 # Initialize the Flask application
 app = Flask(__name__)
